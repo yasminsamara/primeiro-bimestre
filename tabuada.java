dcfa1f5c327bd86b83ee875ff001193e5113142a
yasminsamara <112172983+yasminsamara@users.noreply.github.com>
2022-11-29 16:12:08 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class tabuada {
+	public static void main (String args[]) {
+		Scanner in= new Scanner(System.in);
+		int i=1;
+		int n,r;
+		System.out.println("Digite um número");
+		n=in.nextInt();
+				i = 1;
+				do{
+				    System.out.println(r= n*i);
+				    i++;
+				}while(i <= 10);
+			}
+	}
+
+

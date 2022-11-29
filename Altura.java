a7458bbbc4bb59668c0ad5dbb49e1de8af57f3ba
yasminsamara <112172983+yasminsamara@users.noreply.github.com>
2022-11-29 16:15:00 -0300

    Add files via upload

---
+++ Altura.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Altura {
+	public static void main(String args[]) {
+		Scanner in = new Scanner (System.in);
+		int i=1;
+		double j=34, p=46;
+	    while (j<p) {
+	    	System.out.println(j+2.5);
+	    }
+	    System.out.println("Será mais alto"+1+j);
+		i++;
+	}
+}

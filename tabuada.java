b8df9ef3e164f9afeea4c991b2aeb800e062fee0
yasminsamara <112172983+yasminsamara@users.noreply.github.com>
2022-11-29 16:13:28 -0300

    Add files via upload

--- tabuada.java
+++ tabuada.java
@@ -1,17 +1,18 @@
 import java.util.*;
 public class tabuada {
 	public static void main (String args[]) {
-		Scanner in= new Scanner(System.in);
-		int i=1;
-		int n,r;
-		System.out.println("Digite um número");
-		n=in.nextInt();
-				i = 1;
-				do{
-				    System.out.println(r= n*i);
-				    i++;
-				}while(i <= 10);
-			}
+		Scanner in = new Scanner(System.in);
+        int i=1,n, resultado;
+        
+        System.out.println("Informar um número:");
+        n=in.nextInt();
+             while(i <= 10){
+                 resultado = n*i;
+                 System.out.println(n+"x"+i+"="+resultado);   
+                 i++;
+             }
+             in.close();
+        }
 	}
 
-
+	

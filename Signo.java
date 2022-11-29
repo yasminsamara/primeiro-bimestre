56c25a35dcd0cb0abb4550b24f0961181f72b7fa
yasminsamara <112172983+yasminsamara@users.noreply.github.com>
2022-11-29 16:08:02 -0300

    Add files via upload

---
+++ Signo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Signo {
+	public static void main(String[]args) {
+		Scanner in =new Scanner (System.in);
+		int dia,mes, signo;
+	System.out.println("Digite o seu mês de nascimento");
+	mes=in.nextInt();
+	System.out.println("Digite o dia em que você nasceu");
+	dia=in.nextInt();
+	// Estrutura switch-case
+	switch(mes) {
+	case 1:
+	if (dia<=20);
+	    System.out.println("Capricórnio");
+	}{  
+	    System.out.println("Aquário");
+	break;
+	
+	}
+	}
+
+}
